58e3b99137ea548413df66bf7af2a67ec4a38ba2
Eraklar <wandehousse@gmail.com>
2021-10-31 10:20:13 +0100

    Fail to save notes

--- bloc_note_react/src/App.js
+++ bloc_note_react/src/App.js
@@ -1,31 +1,53 @@
 /* eslint-disable */
 import { useState } from "react";
-import { useEffect } from "react";
-import MarkdownInput from "./components/MarkdownInput";
+import { MarkDownInput } from "./components/MarkdownInput";
 import NoteDisplay from "./components/NoteDisplay";
 import React from "react";
+import { nanoid } from "nanoid";
 
-import NavNoteBar from "./components/NavNoteBar";
+import { NavNoteBar } from "./components/NavNoteBar";
 
 function App() {
-  const [text, setText] = useState('');
-    
-  const handleChange = (event) => {
-        setText(event.target.value);
-  } 
+  const [notes, setNotes] = useState([]);
 
+  const [currentText, setCurrentText] = useState("");
 
-  useEffect(() => {
-    localStorage.setItem("content", JSON.stringify(text));
-  }, [text]);
+  const [currentTitle, setCurrentTitle] = useState("");
 
-  
+  const handleChangeText = (event) => {
+    setCurrentText(event.target.value);
+  };
+
+  const handleChangeTitle = (event) => {
+    setCurrentTitle(event.target.value);
+  };
+
+  const save = (event) => {
+    event.preventDefault();
+    if (!currentText) return;
+    const note = {
+      id: "121313",
+      title: currentTitle,
+      content: currentText,
+    };
+    console.log(note);
+    setNotes([...notes, note]);
+    console.log(notes);
+    setCurrentText("");
+    setCurrentTitle("");
+  };
+
+  //<NavNoteBar notes={notes} />
 
   return (
     <div>
-      <NavNoteBar />
-      <NoteDisplay textToDisplay={text}/>
-      <MarkdownInput text={text} handleChange={handleChange}/>
+      <NoteDisplay textToDisplay={currentText} title={currentTitle} />
+      <MarkDownInput
+        notes={notes}
+        handleChangeText={handleChangeText}
+        handleChangeTitle={handleChangeTitle}
+        save={save}
+      />
     </div>
   );
 }
